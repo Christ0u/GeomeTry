949842b02e8b925be01498e4481cf0f24ba022b0
Danaé A.M. <dev.danae@gmail.com>
2025-01-20 17:11:00 +0100

    ADD: Saut unique

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,21 +7,26 @@ public class Player : MonoBehaviour
     private const float jumpDuration = 0f; // A modifier plus tard
     [SerializeField] Rigidbody2D rb;
 
+    private bool _isGrounded;
+    [SerializeField] Transform groundCheckObject;
+    [SerializeField] private LayerMask layerMask;
+
     void Update()
     {
-        if (Global.playMode)
-        {
-            transform.Translate(new Vector2(speed * Time.deltaTime, 0));
-            Jump();
-        }
+        if (!Global.playMode) return;
+
+        _isGrounded = Physics2D.OverlapCircle(groundCheckObject.position, 0.1f, layerMask);
+
+        transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+        Jump();
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
-            transform.Rotate(new Vector3(0, 0, -360) * jumpDuration * Time.deltaTime);
+            // transform.Rotate(new Vector3(0, 0, -360) * jumpDuration * Time.deltaTime);
         }
     }
 
