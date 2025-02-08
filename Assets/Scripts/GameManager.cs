192aa24338e3f940623909f7bcf0a0a8078661c1
Danaé A.M. <dev.danae@gmail.com>
2025-02-08 17:13:15 +0100

    CHG + ADD : Modification de la méthode de detection de collision #5 et création de prefabs #4

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,21 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static bool playMode = false;
+    public bool playMode = false;
+
+    public void Start()
+    {
+        // StartCoroutine(nameof(LoadScene));
+    }
+    
+    public IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(2);
+        int actualScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(actualScene+1);
+    }
 }
