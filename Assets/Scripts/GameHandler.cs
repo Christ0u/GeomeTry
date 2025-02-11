fd550997cf186dde377cb445665ed874d6a964fa
Christopher GERARD <christopher.gerard@ikmail.com>
2025-02-11 23:55:02 +0100

    CHG : Création et positionnement de GameObjects avec un sprite adéquat pour chaque type d'objet à partir des données issues du fichier JSON
    Signed-off-by: Christopher GERARD <christopher.gerard@ikmail.com>

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -1,24 +1,68 @@
 using UnityEngine;
 using System.IO;
-using Defective.JSON; // Dépendance externe : https://assetstore.unity.com/packages/tools/input-management/json-object-710#description
+using Defective.JSON;
+using System;
+using JetBrains.Annotations;
+using System.Runtime.CompilerServices; // Dépendance externe : https://assetstore.unity.com/packages/tools/input-management/json-object-710#description
 
 public class GameHandler : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Chargement du sprite
+        Sprite blockSprite = Resources.Load<Sprite>("Block");
+        Sprite spikeSprite = Resources.Load<Sprite>("Spike");
+        Sprite smallSpikeSprite = Resources.Load<Sprite>("SpikeSmall");
+
+        // Echelle des sprites
+        float scale = 1.2f;
+
+        // Lecture du fichier JSON
         string mapPath = Application.dataPath + "/Scripts/Map/map.json";
-        //Debug.Log("Chemin vers le fichier map : " + mapPath);
-        
         string rawMap = File.ReadAllText(mapPath);
-        //Debug.Log("Contenu du fichier map : " + rawMap);
 
+        // Parsing du fichier JSON
         JSONObject map = new JSONObject(rawMap);
-        //Debug.Log("Objet JSON : " + map["music"]["path"]);
 
+        int index = 0;
+
+        // Construction de la map
         foreach (JSONObject item in map["map"])
         {
-            Debug.Log(item["type"] + "aux coordonnées (" + item["x"] + "; " + item["y"] + ")");
+            Debug.Log(item["type"].stringValue + " aux coordonnées (" + item["x"].intValue + "; " + item["y"].intValue + ")");
+
+            // Création d'un GameObject
+            GameObject gameItem = new GameObject();
+            gameItem.name = $"{item["type"].stringValue}_" + index;
+
+            // Ajout du sprite au GameObject
+            SpriteRenderer spriteRenderer = gameItem.AddComponent<SpriteRenderer>();
+            if ($"{item["type"].stringValue}" == "spike")
+            {
+                spriteRenderer.sprite = spikeSprite;
+            }
+            else if ($"{item["type"].stringValue}" == "block")
+            {
+                spriteRenderer.sprite = blockSprite;
+            }
+            else if ($"{item["type"].stringValue}" == "smallSpike")
+            {
+                spriteRenderer.sprite = smallSpikeSprite;
+            }
+
+
+            // Positionnement du GameObject
+
+            float xStartPosition = GameObject.Find("Player").transform.position.x; //-10
+            float yStartPosition = GameObject.Find("Player").transform.position.y; //-3.385001
+
+            float x = xStartPosition + (item["x"].intValue * scale);
+            float y = yStartPosition + (item["y"].intValue * scale);
+
+            gameItem.transform.position = new Vector2(x, y);
+
+            index++;
         }
     }
 }
