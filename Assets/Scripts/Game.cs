44165e5dd138e3ed0a7a357a7630e6fbedfaf9e2
Christopher GERARD <christopher.gerard@ikmail.com>
2025-03-17 21:41:29 +0100

    ADD : Implémentation d'une classe Level permettant la construction d'un objet représentant un niveau de jeu logique à partir d'un fichier JSON d'entrée. CHG : Mise à jour de la classe Game prennant en compte l'implémentation de la classe Level ADD : Implémentation d'une classe Utils permettant de charger les prefabs préalablement à la génération d'un niveau de jeu
    Signed-off-by: Christopher GERARD <christopher.gerard@ikmail.com>

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,69 +1,43 @@
 using UnityEngine;
-using Defective.JSON; // Dépendance externe : https://assetstore.unity.com/packages/tools/input-management/json-object-710#description
 using UnityEngine.Tilemaps;
-using System.IO;
-using System.Collections.Generic;
-
 
 public class Game : MonoBehaviour
 {
     public GameObject Character;
-    public GameObject Tile;
-    public GameObject SmallTile;
-    public GameObject Spike;
-    public GameObject SmallSpike;
     public GameObject Ground;
-    public GameObject ShipPortal;
-    public GameObject CubePortal;
-    public GameObject WavePortal;
-    public Tilemap tilemap;
+    public Tilemap Tilemap;
 
-    private Dictionary<string, GameObject> prefabDictionary;
-    private int lastObjectX = int.MinValue;
+    // Propriétés
+    private GameMode _gameMode;
+    private Level _level;
 
     void Start()
     {
-        // Initialisation du dictionnaire de prefabs
-        prefabDictionary = new Dictionary<string, GameObject>
-        {
-            { "tile", Tile },
-            { "smallTile", SmallTile },
-            { "spike", Spike },
-            { "smallSpike", SmallSpike },
-            { "shipPortal", ShipPortal },
-            { "cubePortal", CubePortal },
-            { "wavePortal", WavePortal }
-        };
+        TextAsset jsonFile = Resources.Load<TextAsset>("maps/map");
+        _level = new Level(jsonFile);
+
+        LaunchLevel(_level);
+    }
+
+    // Méthodes
+    void LaunchLevel(Level level)
+    {
+        Debug.Log("Lancement du niveau " + level.Name);
 
         #region Génération de le map
-        // Chargement du fichier JSON
-        TextAsset jsonFile = Resources.Load<TextAsset>("maps/map");
 
-        // Lecture du fichier
-        string rawMap = jsonFile.text;
-
-        // Parsing du fichier JSON
-        JSONObject map = new JSONObject(rawMap);
-
-        //Construction de la map
-        foreach (JSONObject item in map["map"])
+        foreach (MapItem mapItem in level.Map)
         {
-            // Récupération de l'abscisse du dernier objet a instancier
-            if (lastObjectX < item["x"].intValue)
-            {
-                lastObjectX = item["x"].intValue;
-            }
+            Vector3 position = Tilemap.GetCellCenterWorld(new Vector3Int(mapItem.X, mapItem.Y, 0));
 
-            Vector3 position = tilemap.GetCellCenterWorld(new Vector3Int(item["x"].intValue, item["y"].intValue, 0));
-
-            // Ajustement de la position pour que l'objet soit centré sur une case de la tilemap
-            position += new Vector3(tilemap.cellSize.x / 2, tilemap.cellSize.y / 2, 0);
+            //Ajustement de la position pour que l'objet soit centré sur une case de la tilemap
+            position += new Vector3(Tilemap.cellSize.x / 2, Tilemap.cellSize.y / 2, 0);
 
             // Rotation de l'objet
             Quaternion rotation;
-            if (item["rotation"] != null)
+            if (mapItem.Rotation != 0)
             {
-                rotation = Quaternion.Euler(0, 0, item["rotation"].intValue);
+                rotation = Quaternion.Euler(0, 0, mapItem.Rotation);
             }
             else
             {
@@ -72,15 +46,15 @@ public class Game : MonoBehaviour
             }
 
             // Instanciation de l'objet en utilisant le dictionnaire
-            if (prefabDictionary.TryGetValue(item["type"].stringValue, out GameObject prefab))
+            if (Utils.Prefabs.TryGetValue(mapItem.Type, out GameObject prefab))
             {
-                Instantiate(prefab, position, rotation, tilemap.transform);
+                Instantiate(prefab, position, rotation, Tilemap.transform);
             }
         }
 
         // Création d'un objet matérialisant la fin de la map
         GameObject obj = new GameObject("EndOfMap");
-        obj.transform.position = tilemap.GetCellCenterWorld(new Vector3Int(lastObjectX + 1, 0, 0));
+        obj.transform.position = Tilemap.GetCellCenterWorld(new Vector3Int(level.getLastMapItem().X + 1, 0, 0));
         obj.gameObject.tag = "EndOfMap";
 
         #endregion
@@ -88,15 +62,13 @@ public class Game : MonoBehaviour
         #region Génération du personnage et de la caméra
 
         // Instanciation du personnage et de la caméra
-        Instantiate(Character, tilemap.GetCellCenterWorld(new Vector3Int(-10, 1, 0)) + new Vector3(tilemap.cellSize.x / 2, tilemap.cellSize.y / 2, 0), Quaternion.identity, tilemap.transform);
-
+        Instantiate(Character, Tilemap.GetCellCenterWorld(new Vector3Int(-10, 1, 0)) + new Vector3(Tilemap.cellSize.x / 2, Tilemap.cellSize.y / 2, 0), Quaternion.identity, Tilemap.transform);
         Camera cameraInstance = FindFirstObjectByType<Camera>();
         cameraInstance.player = GameObject.Find("CubePrefab(Clone)").transform;
 
         #endregion
 
         #region Génération du sol
-
         int offset = 25;
 
         // Position initiale du personnage
@@ -104,19 +76,19 @@ public class Game : MonoBehaviour
 
         // Définition des coordonnées du sol
         int groundOrigin = characterOrigin - offset;
-        int groundEnd = lastObjectX + offset;
+        int groundEnd = level.getLastMapItem().X + offset;
 
         // Définition de la taille du sol
         int groundSize = groundEnd - groundOrigin;
-        Vector3 groundScale = tilemap.GetCellCenterWorld(new Vector3Int(groundSize, 1, 0));
+        Vector3 groundScale = Tilemap.GetCellCenterWorld(new Vector3Int(groundSize, 1, 0));
 
         // Définitioin de la position du sol
-        Vector3 groundPosition = tilemap.GetCellCenterWorld(new Vector3Int((groundEnd + groundOrigin) / 2, -1, 0)); groundPosition += new Vector3(tilemap.cellSize.x / 2, tilemap.cellSize.y / 2, 0);
+        Vector3 groundPosition = Tilemap.GetCellCenterWorld(new Vector3Int((groundEnd + groundOrigin) / 2, -1, 0));
+        groundPosition += new Vector3(Tilemap.cellSize.x / 2, Tilemap.cellSize.y / 2, 0);
 
         // Instanciation du sol
-        GameObject groundInstance = Instantiate(Ground, groundPosition, Quaternion.identity, tilemap.transform);
+        GameObject groundInstance = Instantiate(Ground, groundPosition, Quaternion.identity, Tilemap.transform);
         groundInstance.transform.localScale = groundScale;
-
         #endregion
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using Defective.JSON; // Dépendance externe : https://assetstore.unity.com/packages/tools/input-management/json-object-710#description
+using System.Collections.Generic;
+
+public class Level
+{
+    // Propriétés
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public int Difficulty { get; private set; }
+    public List<MapItem> Map { get; private set; }
+    private MapItem _lastMapItem;
+
+    // Constructeur
+    public Level(TextAsset mapFile)
+    {
+        // Lecture du fichier
+        string rawData = mapFile.text;
+
+        // Parsing du fichier JSON
+        JSONObject jsonData = new JSONObject(rawData);
+
+        // Enregistrement des propriétés
+        Name = jsonData["name"].stringValue;
+
+        Id = jsonData["id"].intValue;
+
+        Difficulty = jsonData["difficulty"].intValue;
+
+        Map = new List<MapItem>();
+        foreach (JSONObject item in jsonData["map"].list)
+        {
+            MapItem mapItem = new MapItem(
+                item["type"].stringValue,
+                item["x"].intValue,
+                item["y"].intValue,
+                item.HasField("rotation") ? item["rotation"].intValue : 0
+            );
+            Map.Add(mapItem);
+
+            if (mapItem.X > _lastMapItem.X)
+            {
+                _lastMapItem = mapItem;
+            }
+        }
+    }
+
+    public MapItem getLastMapItem()
+    {
+        return _lastMapItem;
+    }
+}
