b44736c3a3981acb14b24dab641135b336bcaa24
Christopher GERARD <christopher.gerard@ikmail.com>
2025-03-21 13:27:47 +0100

    CHG : Uniformisation de la position initiale des prefabs CHG : Adaptation de l'affichage de la barre de progression par rapport à la résolution de l'écran
    Signed-off-by: Christopher GERARD <christopher.gerard@ikmail.com>

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,25 +4,24 @@ using UnityEngine.SceneManagement;
 public class Camera : MonoBehaviour
 {
     private GameManager _gameManager; // A enlever plus tard
-    
+
     [SerializeField] public Transform player;
-    
+
     private float _smoothSpeed = 0.05f;
     private float _startFollowingXPosition = -5.0f; // La caméra commence à suivre le cube à partir de cette position en X
     public Vector3 offset;
     private bool _isFollowing = false;
-    
+
     // A enlever plus tard
     private void Start()
     {
         _gameManager = GameManager.Instance;
-        
+
         if (SceneManager.GetActiveScene().name != "Main Menu" && !_gameManager.PlayMode)
         {
             SceneManager.LoadScene("Main Menu");
         }
     }
-    // -------------------
 
     void FixedUpdate()
     {
@@ -34,15 +33,16 @@ public class Camera : MonoBehaviour
             }
         }
 
-        if(_isFollowing && player != null)
+        if (_isFollowing && player != null)
         {
             var desiredPosition = player.position + new Vector3(5f, 0, -10f); // + offset
             var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
 
-            if (smoothedPosition.y < 4f) {
+            if (smoothedPosition.y < 4f)
+            {
                 smoothedPosition.y = 4f;
             }
-            
+
             transform.position = smoothedPosition;
         }
     }
